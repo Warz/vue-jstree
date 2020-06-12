ccb8e57266443caf205aa1e67a16bc915f822e75
Warz <warzmx@gmail.com>
2020-06-12 02:33:38 +0200

    clean in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -71,28 +71,10 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-/*
- removed in webpack 4:
-      new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),*/
-
-
-    /*
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),*/
 
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-
-
   ]);
 
   module.exports.optimization = {
